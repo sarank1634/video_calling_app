b99bd6daa593efdb57a3c1a4b26298ee872589f2
sarank1634 <saravanakumar1634@gmail.com>
2025-08-17 18:24:19 +0530

    server is running perfectly

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 
 
-app.listen(3001, () => {
+app.get("/", (req,res) => {
+    res.send("hello")
+});
+
+app.listen(5001, () => {
     console.log("server is running onport 5001");
 } )
