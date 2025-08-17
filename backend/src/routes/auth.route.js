2bdf92bf234113a6a0a26188dba0b5ed073eb296
sarank1634 <saravanakumar1634@gmail.com>
2025-08-17 19:21:13 +0530

    controllers folder cretaed after add auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,13 @@
 import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req,res) => {
-    res.send("Signup Router")
-})
+router.get("/signup", signup )
 
-router.get("/login", (req,res) => {
-    res.send("Login Router")
-})
 
-router.get("/logout", (req,res) => {
-    res.send("Logout Router")
-})
+router.get("/login", login)
+
+router.get("/logout", logout)
 
 export default router;
