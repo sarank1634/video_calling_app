576c745777d0bb85c5e172595acb1a6a95f16774
sarank1634 <saravanakumar1634@gmail.com>
2025-08-18 20:00:23 +0530

    chat router hasbeen comleted

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -144,3 +144,23 @@ export async function getOutgoingFriendRequests(req,res) {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+
+
+export async function rejectFriendRequest(req,res){
+    try {
+        const { id: requestId } = req.params;
+        const friendRequest = await FriendRequest.findById(requestId);
+        if (!friendRequest) {
+            return res.status(404).json({ message: "Friend request not found" });
+        }
+        if (friendRequest.recipient.toString() !== req.user.id.toString()) {
+            return res.status(403).json({ message: "You can only reject friend requests sent to you" });
+        }
+        friendRequest.status = "rejected";
+        await friendRequest.save();
+        res.status(200).json({ success: true, message: "Friend request rejected" });
+    } catch (error) {
+        console.log("Error in rejectFriendRequest controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/auth.route.js'
 import userRoutes from "./routes/user.route.js";
+import chatRouter from "./routes/chat.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -13,7 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRouter);
 
 
 app.listen(PORT, () => {
